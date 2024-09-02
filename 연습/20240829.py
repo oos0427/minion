1f4e52ce1de99c1a24fbc187e149d46fe52fb232
oos0427 <oos0427@gmail.com>
2024-09-02 11:42:01 +0900

    커밋

--- 연습/20240829.py
+++ 연습/20240829.py
@@ -1,1 +1,9 @@
-print("hellogit")
+languages = ['python', 'perl', 'c', 'java']
+
+for lang in languages:
+    if lang in ['python', 'perl']:
+        print("%6s need interpreter" % lang)
+    elif lang in ['c', 'java']:
+        print("%6s need compiler" % lang)
+    else:
+        print("should not reach here")
