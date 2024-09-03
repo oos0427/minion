e52910b8620b53b242a87f1750255e3930673cb0
oos0427 <oos0427@gmail.com>
2024-09-03 15:21:42 +0900

    184p

--- 점프 투 파이썬/0903_3(파이썬입출력).py
+++ 점프 투 파이썬/0903_3(파이썬입출력).py
@@ -60,3 +60,46 @@ for i in range(1, 11):
     date = "%d번째 줄입니다.\n" % i
     f.write(date)
 f.close()
+
+# read 함수 사용하기 180p
+f = open("c:/work/새파일.txt", 'r')
+date = f.read()
+print(date)
+f.close()
+
+# 파일 객체를 for 문과 함께 사용하기
+f = open("c:/work/새파일.txt", 'r')
+for line in f:
+    print(line)
+f.close()
+
+# 파일에 새로운 내용 추가하기
+f = open("c:/work/새파일.txt", 'a')
+for i in range(11, 20):
+    data = "%d번째 줄입니다.\n" % i
+    f.write(data)
+f.close()
+
+# with 문과 함께 사용하기
+f = open("c:/work/foo.txt", 'w')
+f.write("Life is short, you need python")
+f.close() #원래라면 이런식으로 열고 닫고를 해줘야한다.
+
+#자동으로 파일이 닫아짐 파일을 열때 경로를 지정하지 않았으므로 Idle파일이 있는 곳에 txt파일이  생성되는 것 확인
+with open("foo.txt", "w") as f:
+    f.write("life is short, you need python") 
+
+#  각 요소를 실행시켜주는 sys파일 만들기 파일명을 sys1.py로 저장해야한다. p184
+import sys
+
+args = sys.argv[1:]
+for i in args:
+    print(i)
+
+#  입력 받은 문자를 대문자로 바꿔주는 프로글매 책에서는 sys2.py로 파일명 설정 p184
+import sys
+
+args = sys.argv[1:]
+for i in args:
+    print(i.upper(), end='')
+
